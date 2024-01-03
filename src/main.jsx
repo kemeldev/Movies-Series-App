76adc8b763b329a2067019f2d380406ba5a2815a
kemeldev <kemel.developer@gmail.com>
2024-01-02 19:48:00 -0600

    Implementing react-Query

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { ApiProvider } from './context/apiContext.jsx'
 import { SearchProvider } from './context/searchContext.jsx'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ApiProvider } from './context/apiContext.jsx'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
 
-    <ApiProvider>
-      <SearchProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </SearchProvider>
-    </ApiProvider>
+  <React.StrictMode>
+    <SearchProvider>
+      <ApiProvider>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      </ApiProvider>
+    </SearchProvider>
   </React.StrictMode>
 )
